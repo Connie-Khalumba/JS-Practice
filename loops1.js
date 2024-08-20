21b76a5225c787391e27c6709c651151dd28538c
constance khalumba <constancekhalumba@gmail.com>
2024-08-20 09:19:06 +0300

    assignments

---
+++ loops1.js
@@ -0,0 +1,8 @@
+function printNumbers(n) {
+    for (let i = 0; i <= n; i++) {
+        console.log(i);
+    }
+}
+
+// Example of nmbers that will be displayed:
+printNumbers(8);
---
+++ loops2.js
@@ -0,0 +1,8 @@
+function doubleArray(numbers) {
+    numbers.forEach(function(number) {
+        console.log(number * 2);
+    });
+}
+
+// Results
+doubleArray([1, 2, 3, 4, 5]);
